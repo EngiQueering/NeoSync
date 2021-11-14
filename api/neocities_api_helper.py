c1a6a92d42d67d947db423812470f6d4646fede3
EngiQueering <dev@engiqueering.com>
2021-11-14 14:57:33 -0500

    add existing files

---
+++ api/neocities_api_helper.py
@@ -0,0 +1,9 @@
+# the url base for an api key request
+BASE_URL = 'https://neocities.org/api/'
+
+# the types of requests
+DELETE = 'delete'
+LIST = 'list'
+UPLOAD = 'upload'
+INFO = 'info'
+KEY = 'key'
